1ee32a2f2f39465bf3e67f1cb1d2d4b3f609060d
Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
2023-07-11 16:26:30 +0200

    Proyecto: cleanup

--- Proyecto/run.sh
+++ Proyecto/run.sh
@@ -4,12 +4,12 @@ set -e
 
 cd $(dirname "$0")
 
-echo "Analyze state_decoder.vhd and tb_state_decoder.vhd"
-ghdl -a state_decoder.vhd
-ghdl -a tb_state_decoder.vhd
+echo "Analyze state_decoder.vhd and tb_state_decoder_cosim.vhd"
+ghdl -a ../Ejercicio/state_decoder.vhd
+ghdl -a tb_state_decoder_cosim.vhd
 
 echo "Build tb (with main.c)"
-ghdl -e -Wl,main.c -o tb_cosim tb_state_decoder
+ghdl -e -Wl,main.c -o tb_cosim tb_state_decoder_cosim
 
 echo "Execute tb"
 ./tb_cosim
